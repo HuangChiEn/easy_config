f5990ee33de6edab41d50dd069fc58c42f976600
joseph <a3285556a@gmail.com>
2023-03-11 22:34:27 +0800

    fix doc conf

--- docs/conf.py
+++ docs/conf.py
@@ -14,21 +14,8 @@ release = 'v1.3.4'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'recommonmark',
-    'sphinx_markdown_tables'
-]
-
-from recommonmark.parser import CommonMarkParser
-source_parsers = {
-    '.md': CommonMarkParser,
-}
-source_suffix = ['.rst', '.md']
-
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
