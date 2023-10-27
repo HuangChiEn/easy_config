c94090d052c525dfab6fb36575cf81a2229cd9d3
joseph <a3285556a@gmail.com>
2023-10-27 15:09:48 +0800

    test for build multi-lang

--- docs/en/source/conf.py
+++ docs/en/source/conf.py
@@ -2,7 +2,7 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
+import os
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -10,7 +10,6 @@ project = 'easy_configer'
 copyright = '2023, JosefHuang'
 author = 'JosefHuang'
 release = 'v2.1.2'
-language = 'zh_TW'  
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -21,5 +20,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+#html_theme = 'sphinx_rtd_theme'
 html_theme = 'renku'
 html_static_path = ['_static']
+html_build_dir = os.environ.get('READTHEDOCS_OUTPUT', 'docs/en/build/html')
--- docs/zh_TW/source/conf.py
+++ docs/zh_TW/source/conf.py
@@ -10,6 +10,7 @@ project = 'easy_configer'
 copyright = '2023, JosefHuang'
 author = 'JosefHuang'
 release = 'v2.1.2'
+language = 'zh_TW'  
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -20,6 +21,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-#html_theme = 'sphinx_rtd_theme'
 html_theme = 'renku'
 html_static_path = ['_static']
+html_build_dir = os.environ.get('READTHEDOCS_OUTPUT', 'docs/zh_TW/build/html')
